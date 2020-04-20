d940b8d6164263c9f34a51b047632d2525a27567
Julián Manrique <ja.manrique@uniandes.edu.co>
2020-04-20 15:00:16 -0500

    Report visualizer v.0001

--- src/report-manager/visualizer/src/views/Home.vue
+++ src/report-manager/visualizer/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Test report visualizer 👨‍💻️</h1>
+    <div class="event-container" v-for="eventID in eventIDs" :key="eventID">
+      [{{eventID}}]
+      <img class="after" :src="(`snapshots/${eventID}/comparison_chrome_vs_firefox_after.png`)">
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
@@ -11,8 +15,34 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      eventIDs: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    };
+  },
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.event-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 8px 0;
+}
+
+.after {
+  height: 240px;
+}
+</style>
